7f4cf234cb47b677d66489535f1bbf334172027b
mincoding <74457149+mincoding1@users.noreply.github.com>
2023-04-28 07:26:58 +0900

    Create Schedule.java

---
+++ java/src/main/java/Schedule.java
@@ -0,0 +1,25 @@
+import java.time.LocalDateTime;
+
+public class Schedule {
+    private LocalDateTime dateTime;
+    private int numberOfPeople;
+    private Customer customer;
+
+    public Schedule(LocalDateTime dateTime, int numberOfPeople, Customer customer) {
+        this.dateTime = dateTime;
+        this.numberOfPeople = numberOfPeople;
+        this.customer = customer;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public int getNumberOfPeople() {
+        return numberOfPeople;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+}
