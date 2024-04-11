b506e548d546bc0d335a913b8b362d04ed7e7beb
mincoding <74457149+mincoding1@users.noreply.github.com>
2024-04-11 20:40:37 +0900

    Update Customer.java

--- java/src/main/java/Customer.java
+++ java/src/main/java/Customer.java
@@ -4,12 +4,12 @@ public class Customer {
     private String phoneNumber;
     private String email;
 
-    public Customer(String name,String phoneNumber) {
+    public Customer(String name, String phoneNumber) {
         this.name = name;
         this.phoneNumber = phoneNumber;
     }
 
-    public Customer(String name,String phoneNumber,String email) {
+    public Customer(String name, String phoneNumber, String email) {
         this.name = name;
         this.phoneNumber = phoneNumber;
         this.email = email;
